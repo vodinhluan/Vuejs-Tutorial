39c4ae88d987bf37343d1bec32b6caa6dba0a26c
vodinhluan <luandinhvo2002@gmail.com>
2023-11-17 16:11:25 +0700

    part2

--- src/App.vue
+++ src/App.vue
@@ -14,5 +14,7 @@ export default {
 
 
 <style>
-  
+  body {
+    background-color:lightslategrey;
+  }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,32 @@
 <template>
   <h1>HelloWorld</h1>
+  <p>Tôi tên là: {{ message }}</p>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      message: "Tèo",
+    };
+  },
+};
+</script>
+
 <style>
-  h1 {
+  h1, p {
     text-align: center;
     color: green;
     background-color: bisque;
   }
 </style>
+
+
+
+
+
+
+
+
+
+
