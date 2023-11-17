c963015f6b9078612b6d1ba7229916905db98ee8
vodinhluan <luandinhvo2002@gmail.com>
2023-11-17 15:38:25 +0700

    component HelloWorld

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,18 @@
 <template>
-  <p>Full Name: {{ formData.firstName }} {{ formData.lastName }}</p>
-  <p>Age: {{ formData.age }}</p>
-
-  <form @submit.prevent="submitForm">
-    <p>
-      <label for="firstName">First Name: </label>
-      <input id="firstName" type="text" v-model.lazy.trim="formData.firstName">
-    </p>
-
-    <p>
-      <label for="lastName">Last Name: </label>
-      <input id="lastName" type="text" v-model="formData.lastName">
-    </p>
-
-    <p>
-      <label for="age">Age: </label>
-      <input id="age" type="number" v-model.number="formData.age">
-    </p>
-
-
-    <button type="submit">Submit</button>
-  </form>
+  <hello-world></hello-world>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld';
+
 export default {
-  data() {
-    return {
-      formData: {
-        firstName: '',
-        lastName: '',
-        age:''
-      }
-    }
-  },
-  methods: {
-    submitForm() {
-      console.log('Submitting form data:', this.formData);
-    }
+  components: { 
+    HelloWorld, 
   }
 }
 </script>
+
+
+<style>
+  
+</style>
